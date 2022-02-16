3c7fe1d9c80359ca2011b822e9ae745ba67b7106
farhanbaig <34104707+farhanbaig@users.noreply.github.com>
2022-02-16 10:47:58 +0000

    Initial Commit

---
+++ AsyncAwaitExample/AsyncAwaitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitExampleApp.swift
+//  AsyncAwaitExample
+//
+//  Created by Farhan Mirza on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
